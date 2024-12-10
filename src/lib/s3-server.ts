eeacf4851b7b6a4f2938e1dae598d742eaf661a1
Jin <jinyung7428@gmail.com>
2024-12-09 19:04:55 -0500

    issue still, move to google ai since billing policy

--- src/lib/s3-server.ts
+++ src/lib/s3-server.ts
@@ -19,9 +19,6 @@ export async function downloadFromS3(file_key: string): Promise<string> {
             const file_name = `/tmp/pdf${Date.now().toString()}.pdf`;
 
             if (obj.Body instanceof require("stream").Readable) {
-                // AWS-SDK v3 has some issues with their typescript definitions, but this works
-                // https://github.com/aws/aws-sdk-js-v3/issues/843
-                //open the writable stream and write the file
                 const file = fs.createWriteStream(file_name);
                 file.on("open", function (fd) {
                     // @ts-ignore
@@ -29,7 +26,6 @@ export async function downloadFromS3(file_key: string): Promise<string> {
                         return resolve(file_name);
                     });
                 });
-                // obj.Body?.pipe(fs.createWriteStream(file_name));
             }
         } catch (error) {
             console.error(error);
@@ -38,5 +34,3 @@ export async function downloadFromS3(file_key: string): Promise<string> {
         }
     });
 }
-
-// downloadFromS3("uploads/1693568801787chongzhisheng_resume.pdf");
